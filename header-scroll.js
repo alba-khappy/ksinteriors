c86247cc9b87eed0c1fafac7850c6b71e39ab117
albinabist1993@mail.ru <b.hapilin@gateline.ru>
2023-04-19 13:57:10 +0500

    правки

--- header-scroll.js
+++ header-scroll.js
@@ -1,19 +1,26 @@
 window.addEventListener('scroll', function() {
 
+    let oldScrollY = 0;
     let header = document.querySelector(".header__section");
     let whatsapp = document.querySelector(".hero-banner__whatsapp");
 
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    let dY = scrolled - oldScrollY;
+
+    if ( dY > 0 ){
+        header.classList.add('header__section--bg');
+    }
+    else if ( window.pageYOffset == 0 ) {
+        header.classList.remove('header__section--bg');
+    } 
+
     if(pageYOffset >= 100) {
-        whatsapp.classList.remove("hero-banner__whatsapp--to-top");
-        header.classList.remove("header__section--background-hide");
-        header.classList.add("header__section--background-show");
         whatsapp.classList.add("hero-banner__whatsapp--to-bottom");
     }
 
     if(pageYOffset < 100) {
-        header.classList.remove("header__section--background-show");
-        header.classList.add("header__section--background-hide");
         whatsapp.classList.remove("hero-banner__whatsapp--to-bottom");
-        whatsapp.classList.add("hero-banner__whatsapp--to-top");
     }
-  });
+
+    oldScrollY = scrolled;
+});
